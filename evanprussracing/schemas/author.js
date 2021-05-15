6e545040e6c8d0f75180fcec20ef58eaaa8fa08f
Rob Wallator <rob17macbook@MacbookPro17.local>
2021-05-14 18:46:15 -0600

    remove node and upgrade sanity

--- evanprussracing/schemas/author.js
+++ evanprussracing/schemas/author.js
@@ -17,27 +17,6 @@ export default {
         maxLength: 96,
       },
     },
-    {
-      name: 'image',
-      title: 'Image',
-      type: 'image',
-      options: {
-        hotspot: true,
-      },
-    },
-    {
-      name: 'bio',
-      title: 'Bio',
-      type: 'array',
-      of: [
-        {
-          title: 'Block',
-          type: 'block',
-          styles: [{title: 'Normal', value: 'normal'}],
-          lists: [],
-        },
-      ],
-    },
   ],
   preview: {
     select: {
